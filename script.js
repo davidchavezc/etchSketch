1ad1cbff76738846450028f1388be8f797e70365
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2024-12-30 09:20:22 -0600

    Fix square drawing loop

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const grid = document.querySelector("#gridContainer");
 let gridSize = 16;
 let gridArea = gridSize**2;
 function drawSquares(area){
-    for(i = 0; area >= i; i++){
+    for(i = 1; area >= i; i++){
         const square = document.createElement("div");
         square.classList.add("square");
         grid.appendChild(square);
