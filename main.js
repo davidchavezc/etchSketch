c0a0b7322be5197ef81a7b741d1d770908752298
davidchavezc <davidchavez1805@outlook.com>
2024-12-31 20:51:21 -0600

    Add show grid button
    Added button to toggle grid line

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector('#grid');
 const slider = document.querySelector('#slider');
 const clear = document.querySelector('#clearButton');
-
+const showGrid = document.querySelector('#toggleGrid');
+let gridShown = 0;
 slider.addEventListener('input', () => {
     grid.innerHTML = '';
     let gridSize = slider.value;
@@ -13,6 +14,7 @@ function drawSquares(area, size) {
     for(i = 1; area >= i; i++){
         const square = document.createElement('div');
         square.classList.add('square');
+        if(gridShown)square.classList.add('outline');
         grid.appendChild(square);
     }
     const squares = document.querySelectorAll('.square');
@@ -33,4 +35,20 @@ clear.addEventListener('click', () => {
     let gridArea = gridSize**2;
     drawSquares(gridArea, gridSize);
 })
+
+showGrid.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    if(gridShown){
+        squares.forEach((square) => {
+            square.classList.remove('outline');
+            gridShown = 0;
+        })
+    }
+    else{
+        squares.forEach((square) => {
+            square.classList.add('outline');
+            gridShown = 1;
+        });
+    }
+});
 drawSquares(256, 16);
