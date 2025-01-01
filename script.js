e358bdd5a917f25faf83d9c1c86f138454772c93
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2024-12-31 18:01:27 -0600

    Fix infinite squares
    Now the slider deletes old square divs ensuring the container does not
    overflow

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const grid = document.querySelector("#gridContainer");
 const sizePicker = document.querySelector('#sizeSlider')
+const squares = document.querySelectorAll('.square')
 let gridSize = sizePicker.value;
 let gridArea = gridSize**2;
-function drawSquares(area){
+function drawSquares(area, size){
     for(i = 1; area >= i; i++){
         const square = document.createElement("div");
         square.classList.add("square");
         grid.appendChild(square);
+        const squares = document.querySelectorAll('.square')
+        squares.forEach(square => {
+            square.style.flex = `1 0 calc(100% / ${size})`
+            square.style.height = `calc(100% / ${size})`
+            console.log(square.style.flex);
+        });
     }
 }
 
 drawSquares(gridArea);
-const squares = document.querySelectorAll('.square')
+sizePicker.addEventListener('input', () => {
+    const squares = document.querySelectorAll('.square')
+    grid.innerHTML = '';
+    let gridSize = sizePicker.value;
+    let gridArea = gridSize**2;
+    drawSquares(gridArea, gridSize);
+});
+
 squares.forEach((square) => {
     square.addEventListener('mouseover', () => {
         square.classList.add('hovered');
     })
 })
-
-sizePicker.addEventListener('input', () => {
-    let gridSize = sizePicker.value;
-    console.log(gridSize);
-    let gridArea = gridSize**2;
-    drawSquares(gridArea);
-});
